7ad2877fcb89297a8543e712f640cc68d3009599
Gabriel Matos <gabriel.ifsal@gmail.com>
2021-10-21 00:34:27 -0300

    Correção na query realizada para recuperar as 3 últimas mensagens criadas

--- src/modules/messages/service/GetLast3MessagesService.ts
+++ src/modules/messages/service/GetLast3MessagesService.ts
@@ -3,7 +3,7 @@ import { prismaClient } from '../../../shared/infra/http/prisma';
 class GetLast3MessagesService {
   async execute() {
     // Get the last 3 messages created
-    const messages = await prismaClient.message.findFirst({
+    const messages = await prismaClient.message.findMany({
       orderBy: { created_at: 'desc' },
       take: 3,
       include: { user: true }
